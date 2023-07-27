956546850a5bae1fd179651784b9694f29273f90
temirlanm <76197653+temirlanmur@users.noreply.github.com>
2023-07-27 13:43:06 +0600

    feat: update dto validator

--- src/ProjectManager.Application/Validators/UpdateProjectDTOValidator.cs
+++ src/ProjectManager.Application/Validators/UpdateProjectDTOValidator.cs
@@ -7,6 +7,8 @@ public class UpdateProjectDTOValidator : AbstractValidator<UpdateProjectDTO>
 {
     public UpdateProjectDTOValidator()
     {
+        RuleFor(d => d.ActorId).NotEmpty();
+        RuleFor(d => d.ProjectId).NotEmpty();
         RuleFor(d => d.Title).Length(3, 255);
         RuleFor(d => d.Description).MaximumLength(2000);
     }
