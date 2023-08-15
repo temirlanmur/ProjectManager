9f6e568ab34a4efb30289ea625da0cd478598579
temirlanm <76197653+temirlanmur@users.noreply.github.com>
2023-08-15 14:03:45 +0600

    feat: add dip methods to Application & Persistence projects

---
+++ src/ProjectManager.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectManager.Application.Interfaces;
+using ProjectManager.Application.Services;
+using System.Reflection;
+
+namespace ProjectManager.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IAuthorizationService, AuthorizationService>();
+        services.AddScoped<IProjectService, ProjectService>();
+        services.AddScoped<ITaskService, TaskService>();
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
---
+++ src/ProjectManager.Persistence/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectManager.Application.Interfaces;
+using ProjectManager.Persistence.Data;
+using ProjectManager.Persistence.Repositories;
+
+namespace ProjectManager.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));            
+        });
+
+        services.AddScoped<IProjectRepository, ProjectRepository>();
+        services.AddScoped<ITaskCommentRepository, TaskCommentRepository>();
+        services.AddScoped<ITaskRepository, TaskRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+}
